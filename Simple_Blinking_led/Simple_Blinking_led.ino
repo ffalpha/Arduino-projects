c2bbd50f0bed8ab6794b1471dd79792a6f9de634
Kalana Mihiranga <31257148+ffalpha@users.noreply.github.com>
2018-09-13 22:22:10 +0530

    Add files via upload

---
+++ Simple_Blinking_led/Simple_Blinking_led.ino
@@ -0,0 +1,15 @@
+
+int led=9; //w9 is the pin number where the postive wire of the led is connected
+void setup() {
+ 
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+ 
+  digitalWrite(led,HIGH);
+  delay(1000);
+   digitalWrite(led,LOW);
+   delay(1000);
+
+}
